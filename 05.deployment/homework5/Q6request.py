d4f0bfcbdf9245200b85532c7a250c1fe6eef9ca
nebellleben <nebeleben@gmail.com>
2025-10-24 03:55:46 +0000

    homework5 update 20251024 3

---
+++ 05.deployment/homework5/Q6request.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "http://0.0.0.0:9696/predict"
+client = {
+    "lead_source": "organic_search",
+    "number_of_courses_viewed": 4,
+    "annual_income": 80304.0
+}
+response = requests.post(url, json=client)
+predictions = response.json()
+
+print(predictions)
